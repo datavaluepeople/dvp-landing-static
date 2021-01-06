7594348c5505990fac59528d606c24f51f3cb814
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-01-06 06:57:33 +0100

    Home: title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,13 @@ import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 import BuilderFooter from '../components/builder-footer/builder-footer.js';
 
+import indexStyles from './index.module.scss';
+
 const LandingPageIndex = ({location}) => {
   return (
     <Layout location={location}>
       <SEO title="We build bespoke state-of-the-art data-driven systems"/>
-      <h1>
+      <h1 className={indexStyles.title}>
         Are you getting returns from your data &amp; analytics?
         <br/>
         We build data-driven systems with <i>measurable</i> value delivered.
