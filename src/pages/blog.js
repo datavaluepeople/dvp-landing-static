60b107cedfc22e8e06aee8bae4d8fbc38ae6ee54
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-01-07 16:41:16 +0100

    track-record-sections.json

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -48,25 +48,13 @@ export default BlogIndex
 
 export const pageQuery = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
+    trackRecordSectionsJson {
+      id
+      paragraph
+      previousClientLink
+      previousClientText
+      title
+      valueDelivered
     }
   }
 `
--- src/pages/track-record.js
+++ src/pages/track-record.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {graphql} from 'gatsby';
 
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
-import TrackRecordSection from '../components/track-record-section/track-record-section';
+import
+TrackRecordSection
+  from '../components/track-record-section/track-record-section';
 
 import trackRecordStyles from './track-record.module.scss';
 
-const TrackRecordPage = ({location}) => {
+const TrackRecordPage = ({data, location}) => {
+  /*
+   * To change the track record sections text change
+   * content/json-config/track-record-sections.json
+   * allTrackRecordSectionsJson queries the file
+   * Query is at bottom of the page
+   */
+  const sections = data.allTrackRecordSectionsJson.edges;
   return (
     <Layout location={location}>
       <SEO title="Track Record"/>
       <h1 className={trackRecordStyles.title}>Track record</h1>
       <div className={trackRecordStyles.content}>
-        <TrackRecordSection
-          iconFile='icon1.svg'
-          title={'Autonomous Optimisation'}
-          paragraph={'Using state-of-the-art reinforcement learning, we make autonomous optimised pricing decisions at scale in near real-time. Our algorithms balance exploiting well performing strategies vs exploring new ones.'}
-          previousClientText={'B2C car rental resale company'}
-          previousClientLink={'www.cartrawler.com'}
-          valueDelivered={'double-figure uplift (%) in revenue, sales, & profit on large sections of business traffic.'}
-        />
+        {sections.map(({key, node}) => {
+          return (
+            <TrackRecordSection
+              key={node.id}
+              iconFile={node.iconFile}
+              title={node.title}
+              paragraph={node.paragraph}
+              previousClientText={node.previousClientText}
+              previousClientLink={node.previousClientLink}
+              valueDelivered={node.valueDelivered}
+            />
+          );
+        })}
       </div>
     </Layout>
   );
@@ -30,4 +45,37 @@ export default TrackRecordPage;
 
 TrackRecordPage.propTypes = {
   location: PropTypes.object,
+  data: PropTypes.shape({
+    allTrackRecordSectionsJson: PropTypes.shape({
+      edges: PropTypes.arrayOf(PropTypes.shape({
+        node: PropTypes.shape({
+          id: PropTypes.String,
+          iconFile: PropTypes.String,
+          title: PropTypes.String,
+          paragraph: PropTypes.String,
+          previousClientText: PropTypes.String,
+          previousClientLink: PropTypes.String,
+          valueDelivered: PropTypes.String,
+        }),
+      })),
+    }),
+  }),
 };
+
+export const query = graphql`
+query {
+  allTrackRecordSectionsJson {
+    edges {
+      node {
+        id
+        iconFile
+        paragraph
+        previousClientLink
+        previousClientText
+        title
+        valueDelivered
+      }
+    }
+  }
+}
+`;
