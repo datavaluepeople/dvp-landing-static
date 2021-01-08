549a6fe7d6e00f1ac245852730b189a315c9c619
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-01-08 17:01:40 +0100

    People: people from yaml

--- src/pages/people.js
+++ src/pages/people.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {graphql} from 'gatsby';
 
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo';
 
 import styles from './people.module.scss';
 
-const PeoplePage = ({location}) => {
+const PeoplePage = ({data, location}) => {
+  const people = data.people.edges;
   return (
     <Layout location={location}>
       <SEO title="People"/>
       <h1 className={styles.title}>People</h1>
       <div className={styles.content}>
+        {people.map(({key, node}) => {
+          return (
+            <div key={node.id}>
+              <h2>{node.fullName}</h2>
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
@@ -21,4 +30,49 @@ export default PeoplePage;
 
 PeoplePage.propTypes = {
   location: PropTypes.object,
+  data: PropTypes.shape({
+    people: PropTypes.shape({
+      edges: PropTypes.arrayOf(PropTypes.shape({
+        node: PropTypes.shape({
+          id: PropTypes.string,
+          bio: PropTypes.string,
+          title: PropTypes.string,
+          fullName: PropTypes.string,
+          github: PropTypes.string,
+          linkedIn: PropTypes.string,
+          profilePicture: PropTypes.object,
+          profilePictureGif: PropTypes.shape({
+            publicURL: PropTypes.string,
+          }),
+        }),
+      })),
+    }),
+  }),
 };
+
+export const PeoplePageQuery = graphql`
+query {
+  people: allPeopleYaml {
+    edges {
+      node {
+        bio
+        email
+        fullName
+        github
+        id
+        linkedIn
+        profilePicture {
+          childrenImageSharp {
+            fluid {
+              src
+            }
+          }
+        }
+        profilePictureGif {
+          publicURL
+        }
+      }
+    }
+  }
+}
+`;
