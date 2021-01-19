ccdefa1b561e557c59e6498a904cbd1d1b7d3e60
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-01-19 17:25:23 +0100

    StaticProps breakpoints

--- src/components/wave/static-props.js
+++ src/components/wave/static-props.js
@@ -2,20 +2,63 @@
  * staticProps calculate
  */
 import colors from '../../styles/colors.scss';
+import breakpoints from '../../styles/breakpoints.scss';
 
 const staticProps = (width, height) => {
+  if (width <= breakpoints.screenMDMin) {
+    return {
+      // Number of curve segments in x
+      segments: 10,
+      // Max growth in px of both + and negavtive
+      growth: 80,
+      // step size for the noise
+      // speed
+      step: 0.008,
+      // Number of lines
+      rows: 10,
+      // The difference in noise between lines
+      lineDiff: 0.15,
+      // The difference in segments I think
+      curveDiff: 0.50,
+      lineWidth: 1,
+      lineColor: colors.dvpWhite,
+      bgColor: colors.dvpDark,
+    };
+  }
+
+  if (width <= breakpoints.screenLGMin) {
+    return {
+      // Number of curve segments in x
+      segments: 15,
+      // Max growth in px of both + and negavtive
+      growth: 80,
+      // step size for the noise
+      // speed
+      step: 0.01,
+      // Number of lines
+      rows: 10,
+      // The difference in noise between lines
+      lineDiff: 0.15,
+      // The difference in segments I think
+      curveDiff: 0.50,
+      lineWidth: 1,
+      lineColor: colors.dvpWhite,
+      bgColor: colors.dvpDark,
+    };
+  }
+
   return {
     // Number of curve segments in x
-    segments: 15,
+    segments: 20,
     // Max growth in px of both + and negavtive
-    growth: 80,
+    growth: 90,
     // step size for the noise
     // speed
     step: 0.01,
     // Number of lines
-    rows: 30,
+    rows: 20,
     // The difference in noise between lines
-    lineDiff: 0.14,
+    lineDiff: 0.15,
     // The difference in segments I think
     curveDiff: 0.50,
     lineWidth: 1,
