403a90088451156c34a5dedf48530f728c8b38ee
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2021-01-25 12:39:38 +0100

    Wave: cross browser support

--- src/components/wave/canvas-context.js
+++ src/components/wave/canvas-context.js
@@ -2,24 +2,33 @@
  * Canvas and context height and width
  *
  */
+import './hidpi-canvas.js';
 
-/*
- * From blog post
- * https://medium.com/wdstack/fixing-html5-2d-canvas-blur-8ebe27db07da
- */
+const getPixelRatio = (window, context) => {
+  const backingStore = context.backingStorePixelRatio ||
+        context.webkitBackingStorePixelRatio ||
+        context.mozBackingStorePixelRatio ||
+        context.msBackingStorePixelRatio ||
+        context.oBackingStorePixelRatio ||
+        context.backingStorePixelRatio || 1;
+
+  return (window.devicePixelRatio || 1) / backingStore;
+};
+
 const getCanvasAndContext = (canvasRef, window, staticProps) => {
   const canvas = canvasRef.current;
   const context = canvas.getContext('2d');
+  const dpr = getPixelRatio(window, context);
   const computedStyle = window.getComputedStyle(canvas);
   const canvasComputedWidth = computedStyle.getPropertyValue('width');
   const canvasWidthProcessed = parseFloat(canvasComputedWidth.slice(0, -2));
   const canvasComputedHeight = computedStyle.getPropertyValue('height');
   const canvasHeightProcessed = parseFloat(canvasComputedHeight.slice(0, -2));
-  const dpr = window.devicePixelRatio;
-  const width = canvasWidthProcessed * dpr;
-  const height = canvasHeightProcessed * dpr;
-  canvas.width = width;
-  canvas.height = height;
+  const canvasWidth = canvasWidthProcessed * dpr;
+  const canvasHeight = canvasHeightProcessed * dpr;
+
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
   return {
     canvas: canvas,
     context: context,
@@ -29,8 +38,8 @@ const getCanvasAndContext = (canvasRef, window, staticProps) => {
     canvasComputedHeight: canvasComputedHeight,
     canvasWidthProcessed: canvasWidthProcessed,
     canvasHeightProcessed: canvasHeightProcessed,
-    canvasWidth: width,
-    canvasHeight: height,
+    canvasWidth: canvas.width,
+    canvasHeight: canvas.height,
     computedDevicePixelRatio: dpr,
   };
 };
