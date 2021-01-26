d3ac095c4111b258bb6955027eff546628a0ffcb
dbcerigo <dbcerigo@gmail.com>
2021-01-26 15:51:19 +0000

    Wave: expand comments for static props

--- src/components/wave/static-props.js
+++ src/components/wave/static-props.js
@@ -1,5 +1,5 @@
 /**
- * staticProps calculate
+ * staticProps that define wave rendering and behaviour
  */
 import colors from '../../styles/colors.scss';
 import breakpoints from '../../styles/breakpoints.scss';
@@ -8,27 +8,33 @@ const staticProps = (canvasProps) => {
   const shared = {
     // Number of curve segments in x
     segments: 5,
-    // Max growth in px of both + and negavtive
+    // Max growth in px in y direction
+    // Effectively wave height
     growth: 80,
-    // step size for the noise
-    // speed
-    step: 0.005,
+    // Step size for the noise, effusively speed
+    step: 0.002,
     // Number of lines
     rows: 50,
-    // The difference in noise between lines
+    // Variability in waves across y dimension, [0,1]
+    // At 1 get fully independent rows (lines)
     lineDiff: 0.1,
-    // The difference in segments I think
+    // Variability in waves across x dimension, [0,1]
+    // At 0 get "planar" waves
     curveDiff: 0.50,
+    // Background color for the canvas
     bgColor: colors.dvpDark,
     // Scaler that will be used to position
     // x1 of the curve
+    // Using anything but 0.5 gives non-symmetrical waves
     x1OffsetScaler: 0.5,
     // The total xOffset that will be used to create
     // visual persective.
     perspectiveOffset: 0,
-    // Line Width should not be below 1 as causes rendering
-    // imperfections
+    // Line Width, [1,10]
+    // Value below 1 causes rendering imperfections
     lineWidth: 1,
+    // Line color, note that color and alpha can make
+    // unexpected to changes to line rendering
     lineColor: colors.dvpWave,
   };
   if (canvasProps.windowWidth <= breakpoints.screenMDMin) {
