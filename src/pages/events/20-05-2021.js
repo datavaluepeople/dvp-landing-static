754135cc152f0649f92ee986a49ec16bb4551c47
dbcerigo <dbcerigo@gmail.com>
2021-05-06 15:23:55 +0100

    Add event name to header

--- src/pages/events/20-05-2021.js
+++ src/pages/events/20-05-2021.js
@@ -11,7 +11,7 @@ const May2021Event = ({location}) => {
       <SEO
         title="20th of May 2021 - people giving value through data"
       />
-      <h1>20th of May 2021</h1>
+      <h1>20th of May 2021 - people giving value through data</h1>
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc6e41SgP4q0E1M9lxJTXrXFfyhgNqCeZPoOjAWbJe7054eMA/viewform?embedded=true" width="100%" height="2000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </Layout>
   );
