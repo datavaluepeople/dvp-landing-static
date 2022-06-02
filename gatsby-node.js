e81366c37ed195570082a9c51a89d7f1c46b60fb
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2022-06-02 15:37:22 +0200

    blog-post page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,58 +1,52 @@
 const {createFilePath} = require(`gatsby-source-filesystem`);
+const path = require(`path`);
 
-/*
- * Removing the blog posts
-const path = require(`path`)
+exports.createPages = async ({graphql, actions, reporter}) => {
+  const {createPage} = actions;
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const result = await graphql(
-    `
+  const blogPost = path.resolve(`./src/templates/blog-post.js`);
+  const result = await graphql(`
       {
         allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
+          sort: { fields: [frontmatter___date], order: ASC }
           limit: 1000
         ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+          nodes {
+            id
+            fields {
+              slug
             }
           }
         }
-      }
-    `
-  )
+      }`,
+  );
 
   if (result.errors) {
-    throw result.errors
+    reporter.panicOnBuild(
+        `There was an error loading your blog posts`,
+        result.errors,
+    );
+    return;
   }
 
   // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
+  const posts = result.data.allMarkdownRemark.nodes;
 
   posts.forEach((post, index) => {
-    const previous = index === posts.length - 1 ? null : posts[index + 1].node
-    const next = index === 0 ? null : posts[index - 1].node
+    const previousPostId = index === 0 ? null : posts[index - 1].id;
+    const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id;
 
     createPage({
-      path: post.node.fields.slug,
+      path: post.fields.slug,
       component: blogPost,
       context: {
-        slug: post.node.fields.slug,
-        previous,
-        next,
+        id: post.id,
+        previousPostId,
+        nextPostId,
       },
-    })
-  })
-}
-*/
+    });
+  });
+};
 
 exports.onCreateNode = ({node, actions, getNode}) => {
   const {createNodeField} = actions;
@@ -70,4 +64,41 @@ exports.onCreateNode = ({node, actions, getNode}) => {
 const express = require('express');
 exports.onCreateDevServer=({app})=>{
   app.use(express.static('public'));
+};
+
+exports.createSchemaCustomization = ({actions}) => {
+  const {createTypes} = actions;
+
+  // Explicitly define the siteMetadata {} object
+  // This way those will always be defined even if removed from gatsby-config.js
+
+  // Also explicitly define the Markdown frontmatter
+  // This way the "MarkdownRemark" queries will return `null` even when no
+  // blog posts are stored inside "content/blog" instead of returning an error
+  createTypes(`
+    type SiteSiteMetadata {
+      author: Author
+      siteUrl: String
+      social: Social
+    }
+    type Author {
+      name: String
+      summary: String
+    }
+    type Social {
+      twitter: String
+    }
+    type MarkdownRemark implements Node {
+      frontmatter: Frontmatter
+      fields: Fields
+    }
+    type Frontmatter {
+      title: String
+      description: String
+      date: Date @dateformat
+    }
+    type Fields {
+      slug: String
+    }
+  `);
 };
