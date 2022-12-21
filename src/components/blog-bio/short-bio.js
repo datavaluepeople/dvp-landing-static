fa0a3b3e5489d99b2799aa51d1e7efa87e2c520c
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2022-12-21 15:55:08 +0100

    Add Link to short bio

--- src/components/blog-bio/short-bio.js
+++ src/components/blog-bio/short-bio.js
@@ -1,14 +1,23 @@
 /**
  * ShortBio component
  */
+import slugify from 'slugify';
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Link} from 'gatsby';
 
 import * as styles from './blog-bio.module.scss';
 
 const ShortBio = ({fullName}) => {
+  const personSlugId = slugify(fullName);
+  const link = `/people#${personSlugId}`;
   return (
-    <span className={styles.shortBio}>Written by {fullName}</span>
+    <span className={styles.shortBio}>
+      Written by&nbsp;
+      <Link className={styles.shortBioLink} to={link}>
+        {fullName}
+      </Link>
+    </span>
   );
 };
 
@@ -16,7 +25,4 @@ export default ShortBio;
 
 ShortBio.propTypes = {
   fullName: PropTypes.String,
-  bio: PropTypes.string,
-  avatar: PropTypes.object,
-  email: PropTypes.string,
 };
