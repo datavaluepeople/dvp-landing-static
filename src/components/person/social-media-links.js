6f6a842e8b135ad78d35043ceaaa05ac8c59ece9
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2022-12-21 15:25:23 +0100

    Resolve the PersonSocialMediaLinks parents height
    The height of the parent elements (a, div) where bigger then
    the image. This change resolves that.

--- src/components/person/social-media-links.js
+++ src/components/person/social-media-links.js
@@ -4,10 +4,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Theses styles are needed to make sure the
+ * parent elements [a|div] are the same height
+ * as the image.
+ */
+const generalStyles = {
+  lineHeight: 0,
+  display: "inline-block"
+}
+
+
 const PersonSocialMediaLinks = ({className, github, linkedIn}) => {
   return (
-    <div className={className}>
+    <div className={className} style={generalStyles}>
       <a
+        style={generalStyles}
         href={`https://www.linkedin.com/in/${linkedIn}/`}
         target="_blank"
         rel="noreferrer noopener"
@@ -18,6 +30,7 @@ const PersonSocialMediaLinks = ({className, github, linkedIn}) => {
         />
       </a>
       <a
+        style={generalStyles}
         href={`https://github.com/${github}/`}
         target="_blank"
         rel="noreferrer noopener"
