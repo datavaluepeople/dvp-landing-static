14e14fd32e2074456c0e887aefae129741e0c1e8
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2023-10-25 10:25:41 +0200

    Improve the calculation of the person placeholder height
    This refactors some poo poo code that I wrote that didn't work.

--- src/components/person/person.js
+++ src/components/person/person.js
@@ -24,7 +24,6 @@ const Person = (
     },
 ) => {
   const placeholderImage = gifPlaceHolder.childrenImageSharp[0].gatsbyImageData;
-  const minLargeHeight = placeholderImage.height;
   let gifImage;
   if (profilePictureGifUrl) {
     gifImage = (
@@ -37,23 +36,46 @@ const Person = (
   } else {
     gifImage = '';
   }
-  const ratio = (
-    sharedPeopleStyles.personImgMdWidth / sharedPeopleStyles.personImgXlgWidth
+  /*
+   * There is currently a small styling problem that is currently fixed
+   * in a hacky way.
+   *
+   * The people gif is lazy loaded so is added to the HTML after the page
+   * has loaded. Futher more there is a placeholder image that has to have
+   * position absolute. This means that before the gif is loaded the hight
+   * of the flex box is not correct and person containers overlap.
+   *
+   * To solve this we set the min-height of the placeholder image to be
+   * the same as the gif for md and xlg screens.
+   *
+   * Since the gifs height is related to the width of the image as set in
+   * src/styles/shared/people-variables.module.scss.
+   *
+   * We then work out the actual height computing ratio
+   * that the width has been changed for each display screen
+   * and multiplying the height by this.
+   *
+   * The width is set in:
+   * src/styles/shared/people.scss
+   * and these are passed as variables to the js in the file:
+   * src/styles/shared/people-variables.module.scss
+   *
+   * IMPORTANT: for this to work the placeholder has to be the same
+   * width and height as the gif.
+   */
+  const multiplierXLgWidth = (
+    sharedPeopleStyles.personImgXlgWidth / placeholderImage.width
   );
-  const minSmallHeight = placeholderImage.height * ratio;
+  const minLargeHeight = placeholderImage.height * multiplierXLgWidth;
+  const multiplierMdWidth = (
+    sharedPeopleStyles.personImgMdWidth / placeholderImage.width
+  );
+  const minSmallHeight = placeholderImage.height * multiplierMdWidth;
   const personSlugId = slugify(fullName);
 
   return (
     <div id={personSlugId} className={styles.container}>
-      {/* The Gatsby image that is used as the placeholder
-        has style position absolute. This means that sometimes
-        the image goes over the height of the flex box.
-
-        We write some min height embedded CSS so that the min height
-        is correct for that image.
-
-        This is done for md and large screens.
-      */}
+      {/* Here we add the styling as inline for the different screens.*/}
       <style dangerouslySetInnerHTML={{__html: `
          @media (min-width: 768px) {
             #${personSlugId} .${styles.imgContainer} {
