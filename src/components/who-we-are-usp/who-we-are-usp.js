507c4e93480ba3fdeffeed3662659e366f40f8d3
Benjamin Cerigo <benjamin.cerigo@gmail.com>
2025-08-05 11:10:13 +0200

    Add basic who we are USP

---
+++ src/components/who-we-are-usp/who-we-are-usp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import * as whoWeAreStyles from './who-we-are-usp.module.scss';
+
+
+const WhoWeAreUsp = ({HomePage}) => {
+  const pageClass = HomePage ?
+    whoWeAreStyles.homePageElement :
+    whoWeAreStyles.sideBarElement;
+
+  return (
+    <div
+      className={`
+        ${pageClass}
+      `}
+    >
+      <p>
+        We’re a trusted team of senior AI and data specialists.
+        We move fast, ship production systems, and embed directly with client teams.
+        No layers of management - work directly with us in tight weekly development cycles.
+        Lean, high-ROI, collaborative problem-solving with measurable results.
+      </p>
+    </div>
+  );
+};
+
+export default WhoWeAreUsp;
+
+WhoWeAreUsp.propTypes = {
+  HomePage: PropTypes.bool,
+};
